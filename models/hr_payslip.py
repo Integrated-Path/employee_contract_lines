157a12a6d1efca7403ed9d38bd9c01f80e7410f7
Hayder <hadermusc@gmai.com>
2021-08-23 09:30:32 +0000

    Fix hours error

--- models/hr_payslip.py
+++ models/hr_payslip.py
@@ -9,11 +9,12 @@ class HrPayslip(models.Model):
 
     attendance_hours = fields.Float(compute='_compute_attendance_hours', store=True)
 
+    @api.depends('worked_days_line_ids')
     def _compute_attendance_hours(self):
         for record in self:
             for line in record.worked_days_line_ids:
                 if line.work_entry_type_id.name == "Attendance":
                     record.attendance_hours = line.number_of_hours
-                    break
                 else:
                     record.attendance_hours = 0
+                break
